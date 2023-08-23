c1db35f1b500e4ca9293ab5d7f19faaddbb73363
youyeul301 <84696773+youyeul301@users.noreply.github.com>
2023-08-23 17:28:13 +0900

    Add files via upload

---
+++ react/12_react/01_lecture-source/05_router/01_react-router-config/src/pages/About.js
@@ -0,0 +1,9 @@
+function About(){
+    return (
+        <diV>
+            <h1>소개</h1>
+        </diV>
+    )
+}
+
+export default About;
---
+++ react/12_react/01_lecture-source/05_router/01_react-router-config/src/pages/Main.js
@@ -0,0 +1,9 @@
+function Main(){
+    return (
+        <div>
+            <h1>메인</h1>
+        </div>
+    )
+}
+
+export default Main;
